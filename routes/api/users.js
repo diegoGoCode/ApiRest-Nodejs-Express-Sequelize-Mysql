a3f957a817b092f8c886f7c71a25ac01746ed556
DiegoGoCode <diegoalejandrodiazpaez@gamil.com>
2020-07-31 01:54:18 -0500

    Se añadio una autenticacion por tokens para los servicios expuestos para las peliculas

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const { User } = require('../../db');
 const { check, validationResult } = require('express-validator');
+const moment = require('moment');
+const jwt = require('jwt-simple');
 
 router.post('/register', [
     check('username', 'El nombre de usuario es requerido').not().isEmpty(),
@@ -19,5 +21,28 @@ router.post('/register', [
     const user = await User.create(req.body);
     res.json(user);
 });
+
+router.post('/login', async (req, res) => {
+    const user = await User.findOne({ where: { email: req.body.email }});
+    if(user){
+        const iguales = bcrypt.compareSync(req.body.password, user.password);
+        if(iguales){
+            res.json({success: createToken(user)})
+        }else{
+            res.json({error: 'Error en usuario y/o contraseña'});
+        }
+    }else{
+        res.json({error: 'Error en usuario y/o contraseña'});
+    }
+});
+
+const createToken = (user) => {
+    const payload = {
+        usuarioId: user.id,
+        createdAt: moment().unix(),
+        expiredAt: moment().add(5, 'minutes').unix()
+    }
+    return jwt.encode(payload, 'frase secreta')
+}
 
 module.exports = router;
